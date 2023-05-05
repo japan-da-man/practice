a208819042c2cc2f4d81d6e628db3cfeaa280e72
japan-da-man <suzukinao.nao.nao.nao@gmail.com>
2023-05-05 15:26:47 +0900

    5/5

--- selfproduce/src/index.js
+++ selfproduce/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import FullCalendar from '@fullcalendar/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <FullCalendar
+  events={[
+    { title: "event 1", start: "2021-06-01" }, // endは省略可
+    { title: "event 2", start: "2021-06-03", end: "2021-06-05" }, // endに指定した日付は含まないので注意
+    {
+      title: "event 3",
+      start: "2021-06-04T10:00:00", // 時間を指定するときはISO 8601
+    },
+  ]}
+  />
 );
