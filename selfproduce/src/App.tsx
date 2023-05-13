437cdafe2c75ea48422cd39bfe56ef7b426006d4
japan-da-man <suzukinao.nao.nao.nao@gmail.com>
2023-05-13 15:14:31 +0900

    5/13

--- selfproduce/src/App.tsx
+++ selfproduce/src/App.tsx
@@ -1,39 +1,39 @@
-// import React, { ChangeEvent, useState } from 'react';
-// import './App.css';
+import React, { ChangeEvent, useState } from 'react';
+import './App.css';
 
-// function App() {
-//   const [text, setText] = useState<string>("");
-//   const [task, setTask] = useState<string[]>([]);
+function App() {
+  const [text, setText] = useState<string>("");
+  const [task, setTask] = useState<string[]>([]);
 
-//   const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
-//     setText(e.target.value);
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
+    setText(e.target.value);
 
-//   const onClickAdd = () => {
+  const onClickAdd = () => {
 
-//     const newTask = [...task];
+    const newTask = [...task];
 
-//     newTask.push(text);
-//     setTask(newTask);
-//     setText("");
-//   };
+    newTask.push(text);
+    setTask(newTask);
+    setText("");
+  };
 
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
---
+++ selfproduce/src/index.ts
@@ -0,0 +1,10 @@
+import React, { useCallback, useState, useEffect } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+const CalendarData = (props) => {
+  return (
+    
+  );
+};
+export default CalendarData;
